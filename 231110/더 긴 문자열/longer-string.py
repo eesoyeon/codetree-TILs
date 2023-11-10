fce37de2b07ea0c52032330a1660d190a0598c78
Soyeon Lee <102416278+eesoyeon@users.noreply.github.com>
2023-11-10 19:18:24 +0900

    Title: 더 긴 문자열, Time: 115ms, Memory: 32MB, Status: Passed - CodeTree

--- 231110/더 긴 문자열/longer-string.py
+++ 231110/더 긴 문자열/longer-string.py
@@ -1,5 +1,9 @@
 str1, str2 = input().split()
 
-1
 
-print(str1, len(str1)) if len(str1)>len(str2) else print(str2, len(str2))
+if len(str1)>len(str2):
+    print(str1, len(str1))  
+elif len(str1)<len(str2):
+    print(str2, len(str2))
+else:
+    print("same")
