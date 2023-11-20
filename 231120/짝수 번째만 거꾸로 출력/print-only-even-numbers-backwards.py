be2d2eacf63ce5e13c3d9c4f1a0a888d22efacc9
Soyeon Lee <102416278+eesoyeon@users.noreply.github.com>
2023-11-20 17:50:56 +0900

    Title: 짝수 번째만 거꾸로 출력, Time: 137ms, Memory: 24MB, Status: Passed - CodeTree

--- 231120/짝수 번째만 거꾸로 출력/print-only-even-numbers-backwards.py
+++ 231120/짝수 번째만 거꾸로 출력/print-only-even-numbers-backwards.py
@@ -4,5 +4,5 @@ if len(string)%2==0:
     for elem in string[::-2]:
         print(elem, end="")
 else:
-    for elem in string[len(string)-1::-2]:
+    for elem in string[len(string)-2::-2]:
         print(elem, end="")
