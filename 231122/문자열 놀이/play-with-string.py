fda1abd5cfa09765ed7dd7acb6f5297146b9e6a0
Soyeon Lee <102416278+eesoyeon@users.noreply.github.com>
2023-11-22 19:25:54 +0900

    Title: 문자열 놀이, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 231122/문자열 놀이/play-with-string.py
+++ 231122/문자열 놀이/play-with-string.py
@@ -7,9 +7,8 @@ for _ in range(q):
     arr = list(input().split())
     arr[0] = int(arr[0])
     if arr[0] == 1:
-        temp = s[int(arr[1])-1]
-        s[int(arr[1])-1] = s[int(arr[2])-1]
-        s[int(arr[2])-1] = temp
+        a, b = int(arr[1]), int(arr[2])
+        s[a-1], s[b-1] = s[b-1], s[a-1]
         print(''.join(s))
     elif arr[0] == 2:
         s = ''.join(s)
