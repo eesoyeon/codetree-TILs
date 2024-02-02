6587ebd52e99557b9953452c7815129e4796e7a6
Soyeon Lee <102416278+eesoyeon@users.noreply.github.com>
2024-02-02 19:02:45 +0900

    Title: n개의 숫자 중 최대 2개, Time: 201ms, Memory: 24MB, Status: Passed - CodeTree

--- 240202/n개의 숫자 중 최대 2개/two-max-of-n-num.py
+++ 240202/n개의 숫자 중 최대 2개/two-max-of-n-num.py
@@ -3,3 +3,14 @@ arr = list(map(int, input().split()))
 
 arr.sort()
 print(arr[-1], arr[-2])
+
+# if arr[0]>arr[1]:
+#     max1, max2 = arr[0], arr[1]
+# else:
+#     max2, max1 = arr[0], arr[1]
+
+# for i in range(2, n):
+#     if arr[i] >= max1:
+#         max2, max1 = max1, arr[i]
+#     elif arr[i] >= max2:
+#         max2 = arr[i]
