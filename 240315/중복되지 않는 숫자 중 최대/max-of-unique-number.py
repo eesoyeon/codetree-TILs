d6401ac633ad7c368f729272a732435bbf81255b
Soyeon Lee <102416278+eesoyeon@users.noreply.github.com>
2024-03-15 13:18:10 +0900

    Title: 중복되지 않는 숫자 중 최대, Time: 130ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240315/중복되지 않는 숫자 중 최대/max-of-unique-number.py
@@ -0,0 +1,13 @@
+n = int(input())
+arr = list(map(int, input().split()))
+
+answer = []
+dic = {}
+for a in arr:
+    dic[a] = arr.count(a) 
+
+for key in dic:
+    if dic[key] == 1:
+        answer.append(key)
+
+print(max(answer) if answer!=[] else -1)
