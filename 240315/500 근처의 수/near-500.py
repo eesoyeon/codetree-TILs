8f4623d98c883f967498f1c3a6c513a68d6ecae1
Soyeon Lee <102416278+eesoyeon@users.noreply.github.com>
2024-03-15 13:59:53 +0900

    Title: 500 근처의 수, Time: 203ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240315/500 근처의 수/near-500.py
@@ -0,0 +1,11 @@
+arr = list(map(int, input().split()))
+
+max_a, min_a = 0, 1000
+
+for a in arr:
+    if a<500 and max_a<a:
+        max_a = a
+    if a>500 and min_a>a:
+        min_a = a
+
+print(max_a, min_a)
