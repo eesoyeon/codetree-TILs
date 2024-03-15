70367bf5b06dc8887512c796dc82ec348003ac72
Soyeon Lee <102416278+eesoyeon@users.noreply.github.com>
2024-03-15 16:46:45 +0900

    Title: 미는 횟수, Time: 116ms, Memory: 25MB, Status: Passed - CodeTree

--- 240315/미는 횟수/number-of-pushes.py
+++ 240315/미는 횟수/number-of-pushes.py
@@ -5,11 +5,13 @@ n = 0
 # print(A[:-1])
 for i in range(99):
     A = A[-1] + A[:-1]
-    if A == B:
-        n = i+1
+    if A==B:
+        n += 1
         break
+    n += 1
 
-if n<100:
+
+if n < 99:
     print(n)
 else:
     print(-1)
