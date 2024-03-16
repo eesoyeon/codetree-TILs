3eb8115613bcdc136f95c9dfebc47d24a6c881fb
Soyeon Lee <102416278+eesoyeon@users.noreply.github.com>
2024-03-16 16:49:58 +0900

    Title: k번째로 신기한 문자열, Time: 138ms, Memory: 24MB, Status: Passed - CodeTree

--- 240316/k번째로 신기한 문자열/kth-special-string.py
+++ 240316/k번째로 신기한 문자열/kth-special-string.py
@@ -1,11 +1,26 @@
 n, k, T = input().split()
 n, k = int(n), int(k)
-words = [input() for _ in range(n)]
-ans = []
+# words = [input() for _ in range(n)]
+# ans = []
+
+# words.sort()
+# for word in words:
+#     if word[:len(T)] == T:
+#         ans.append(word)
+
+# print(ans[k-1])
+
+
+def starts_with(a, b):
+    if len(a) < len(b):
+        return False
+    return a[:len(b)] == b
+
+words = []
+for _ in range(n):
+    word = input()
+    if starts_with(word, T):
+        words.append(word)
 
 words.sort()
-for word in words:
-    if word[:len(T)] == T:
-        ans.append(word)
-
-print(ans[k-1])
+print(words[k-1])
