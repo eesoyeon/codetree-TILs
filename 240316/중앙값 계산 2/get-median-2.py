4f21a5b9a1ca187da71feba69616c733e5538e92
Soyeon Lee <102416278+eesoyeon@users.noreply.github.com>
2024-03-16 17:01:25 +0900

    Title: 중앙값 계산 2, Time: 108ms, Memory: 24MB, Status: Passed - CodeTree

--- 240316/중앙값 계산 2/get-median-2.py
+++ 240316/중앙값 계산 2/get-median-2.py
@@ -2,13 +2,11 @@ n = int(input())
 li = list(map(int, input().split()))
 
 ans = []
-for i in range(n):
-    x = []
-    if li[i]%2!=0:
-        x = sorted(li[:i+1])
-        ans.append(x[i//2])
-        # ans.append(li[])
-        # x.append(li[:i+1])
+for i in range(0, n+1, 2):
+    x = sorted(li[:i+1])
+    ans.append(x[i//2])
+    # ans.append(li[])
+    # x.append(li[:i+1])
 
 for elem in ans:
     print(elem, end=" ")
