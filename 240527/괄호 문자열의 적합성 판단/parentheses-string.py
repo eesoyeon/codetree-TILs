a2ccc4a9c5d6bb0b0514da9bd14010128499c6ad
Soyeon Lee <102416278+eesoyeon@users.noreply.github.com>
2024-05-27 17:33:35 +0900

    Title: 괄호 문자열의 적합성 판단, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240527/괄호 문자열의 적합성 판단/parentheses-string.py
+++ 240527/괄호 문자열의 적합성 판단/parentheses-string.py
@@ -2,14 +2,15 @@ li = list(input())
 
 stack_li = []
 
+answer = 'Yes'
 for i in li:
     if i=='(':
         stack_li.append(i)
     else:
-        if stack_li!=[]:
+        if stack_li==[]:
+            answer = 'No'
+            break
+        else:
             stack_li.pop()
 
-if stack_li==[]:
-    print("Yes")
-else:
-    print('No')
+print(answer)
